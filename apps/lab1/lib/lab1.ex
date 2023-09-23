6e4840fde14fa088d155ef7380bbc51c8ff4a610
kamilchulakov <mr1english4@gmail.com>
2023-09-23 18:35:56 +0300

    LAB1: problem 4 | small improvements

--- apps/lab1/lib/lab1.ex
+++ apps/lab1/lib/lab1.ex
@@ -11,9 +11,10 @@ defmodule Lab1 do
   end
 
   defp is_palindrome_product(x, y), do: x * y |> is_palindrome
-  
+
+  @spec largest_palindrome_product_of_3digit_numbers :: integer
   def largest_palindrome_product_of_3digit_numbers do
-    for x <- 999..100, y <- 999..100,
+    for x <- 999..100, y <- x..100,
       is_palindrome_product(x, y) do x * y
     end
       |> Enum.max()
--- apps/lab1/test/lab1_test.exs
+++ apps/lab1/test/lab1_test.exs
@@ -2,7 +2,7 @@ defmodule Lab1Test do
   use ExUnit.Case
   doctest Lab1
 
-  test "largest palindrome product" do
+  test "largest palindrome product of 3-digit numbers" do
     assert Lab1.largest_palindrome_product_of_3digit_numbers() == 906609
   end
 end
