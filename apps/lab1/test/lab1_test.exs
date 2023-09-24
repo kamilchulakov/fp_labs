4d16ef3bd9ef774c08e09d9cf3598e4955e147df
kamilchulakov <mr1english4@gmail.com>
2023-09-24 13:06:09 +0300

    LAB1: fix code issue

--- apps/lab1/test/lab1_test.exs
+++ apps/lab1/test/lab1_test.exs
@@ -3,6 +3,6 @@ defmodule Lab1Test do
   doctest Lab1
 
   test "largest palindrome product of 3-digit numbers" do
-    assert Lab1.largest_palindrome_product_of_3digit_numbers() == 906609
+    assert Lab1.largest_palindrome_product_of_3digit_numbers() == 906_609
   end
 end
