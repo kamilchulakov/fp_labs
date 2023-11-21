187600c4b05e3e5d21828c81c449fa77596d9ac0
Kamil Chulakov <mr1english4@gmail.com>
2023-11-22 00:16:02 +0300

    LAB1: refactor streams solution

--- apps/lab1/lib/stream4.ex
+++ apps/lab1/lib/stream4.ex
@@ -4,18 +4,32 @@ defmodule Stream4 do
 
   @min_num 100
   @max_num 999
+  @min_max_range @min_num..@max_num
 
   @spec largest_palindrome_product_of_3digit_numbers :: integer
   def largest_palindrome_product_of_3digit_numbers do
-    range_cycle = Stream.cycle(@min_num..@max_num)
+    range_cycle = Stream.cycle(@min_max_range)
+    range_to_x = fn x -> Stream.take(range_cycle, x - @min_num) end
+
+    map_to_product = fn stream, x -> Stream.map(stream, fn y -> x * y end) end
+
+    max_product = fn stream ->
+      Enum.reduce(stream, nil, fn product, acc ->
+        cond do
+          acc == nil -> product
+          product > acc -> product
+          true -> acc
+        end
+      end)
+    end
 
     range_cycle
     |> Stream.take(@max_num - @min_num + 1)
     |> Stream.flat_map(fn x ->
-        Stream.take(range_cycle, x - @min_num)
-        |> Stream.map(fn y -> x * y end)
-      end)
+      range_to_x.(x)
+      |> map_to_product.(x)
+    end)
     |> Stream.filter(fn product -> is_palindrome(product) end)
-    |> Enum.max()
+    |> max_product.()
   end
 end
