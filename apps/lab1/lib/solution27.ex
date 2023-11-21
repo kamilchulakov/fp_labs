39d2a4a362b701cc2383431174e62ff36b58daa6
Kamil Chulakov <mr1english4@gmail.com>
2023-11-22 00:15:27 +0300

    LAB1: format

--- apps/lab1/lib/solution27.ex
+++ apps/lab1/lib/solution27.ex
@@ -1,5 +1,7 @@
 defmodule Solution27 do
   @moduledoc false
+  @limit_a -1000..999
+  @limit_b -1000..1000
 
   defp calc_by_formula(n, a, b) do
     n * n + a * n + b
@@ -16,9 +18,7 @@ defmodule Solution27 do
   defp comparator([n, _, _]), do: n
 
   defp find_best do
-    limit_a = -1000..999
-    limit_b = -1000..1000
-    for a <- limit_a, b <- limit_b do
+    for a <- @limit_a, b <- @limit_b do
       max_continuous_prime(0, a, b)
     end
     |> Enum.max_by(&comparator(&1), fn -> nil end)
@@ -27,6 +27,6 @@ defmodule Solution27 do
   def solve do
     find_best()
     |> Enum.drop(1)
-    |> Enum.reduce(1, fn (x, acc) -> x * acc end)
+    |> Enum.reduce(1, fn x, acc -> x * acc end)
   end
 end
