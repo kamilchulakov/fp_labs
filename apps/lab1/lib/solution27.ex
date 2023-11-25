836fc9f04c81ac08eca38d9a0fc84bf334f75ed4
Kamil Chulakov <mr1english4@gmail.com>
2023-11-25 20:01:58 +0300

    LAB1: improve code

--- apps/lab1/lib/solution27.ex
+++ apps/lab1/lib/solution27.ex
@@ -1,13 +1,12 @@
 defmodule Solution27 do
   @moduledoc false
+
   @limit_a -1000..999
   @limit_b -1000..1000
 
-  defp calc_by_formula(n, a, b) do
-    n * n + a * n + b
-  end
+  defp calc_by_formula(n, a, b), do: n * n + a * n + b
 
-  defp max_continuous_prime(n, a, b) do
+  defp max_continuous_prime(n \\ 0, a, b) do
     if ElixirMath.PrimeGenerator.is_prime(calc_by_formula(n, a, b)) do
       max_continuous_prime(n + 1, a, b)
     else
@@ -15,18 +14,16 @@ defmodule Solution27 do
     end
   end
 
-  defp comparator([n, _, _]), do: n
-
   defp find_best do
     for a <- @limit_a, b <- @limit_b do
-      max_continuous_prime(0, a, b)
+      max_continuous_prime(a, b)
     end
-    |> Enum.max_by(&comparator(&1), fn -> nil end)
+    |> Enum.max_by(&(&1))
   end
 
   def solve do
     find_best()
     |> Enum.drop(1)
-    |> Enum.reduce(1, fn x, acc -> x * acc end)
+    |> Enum.reduce(1, &(&1 * &2))
   end
 end
