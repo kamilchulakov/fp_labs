586d9af0d2ba06f8c5e4128a005abc8baba8e62c
Kamil Chulakov <mr1english4@gmail.com>
2024-02-08 16:52:52 +0300

    LAB3: GenStage stub

---
+++ apps/lab3/lib/lab3/application.ex
@@ -0,0 +1,20 @@
+defmodule Lab3.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Lab3.Worker.start_link(arg)
+      # {Lab3.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Lab3.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
