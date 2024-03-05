11c7a3f4fe021e60120304e2e6cfff8a31b42618
Kamil Chulakov <mr1english4@gmail.com>
2024-03-06 00:42:12 +0300

    LAB4: check type before filter

--- apps/lab4/lib/lab4/db/filter.ex
+++ apps/lab4/lib/lab4/db/filter.ex
@@ -2,26 +2,40 @@ defmodule Lab4.DB.Filter do
   def filter(data, filter)
 
   def filter(data, {:more, value}) do
-    Stream.filter(data, entry_filter(value: &(&1 > value)))
+    Stream.filter(data, entry_filter(:integer, value: &(&1 > value)))
   end
 
   def filter(data, {:less, value}) do
-    Stream.filter(data, entry_filter(value: &(&1 < value)))
+    Stream.filter(data, entry_filter(:integer, value: &(&1 < value)))
   end
 
   def filter(data, {:starts_with, value}) do
-    Stream.filter(data, entry_filter(value: &String.starts_with?(&1, value)))
+    Stream.filter(data, entry_filter(:string, value: &String.starts_with?(&1, value)))
   end
 
   def filter(data, {:ends_with, value}) do
-    Stream.filter(data, entry_filter(value: &String.ends_with?(&1, value)))
+    Stream.filter(data, entry_filter(:string, value: &String.ends_with?(&1, value)))
   end
 
   def matches?(entry, filter) do
     filter([entry], filter) == [entry]
   end
 
-  defp entry_filter(value: filter) do
-    fn {_, value} -> filter.(value) end
+  defp entry_filter(:string, value: filter) do
+    fn {_, value} ->
+      cond do
+        is_bitstring(value) -> filter.(value)
+        true -> false
+      end
+    end
+  end
+
+  defp entry_filter(:integer, value: filter) do
+    fn {_, value} ->
+      cond do
+        is_integer(value) -> filter.(value)
+        true -> false
+      end
+    end
   end
 end
